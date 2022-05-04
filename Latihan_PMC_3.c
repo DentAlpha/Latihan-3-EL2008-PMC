3414f6a1b6663087669fdd236892dce5449fc749
DentAlpha <82549965+DentAlpha@users.noreply.github.com>
2022-05-04 17:18:31 +0700

    Update program cetak NIM dan nama mahasiswa
    Penambahan bagian validasi jumlah mahasiswa dan validasi persen kehadiran (tidak termasuk input dengan tipe data yang tidak diinginkan)

--- Latihan_PMC_3.c
+++ Latihan_PMC_3.c
@@ -5,33 +5,48 @@ Nama : Surya Dharma
 NIM  : 13220027
 */
 
+/*Asumsi input sesuai dengan tipe data yang diminta
+*/
+
 struct mahasiswa {
     char nama[50];
     int nim;
     int kehadiran;
 };
 
-void main()
+int main()
 {
     int N;
     printf("Jumlah mahasiswa : ");
     scanf("%d", &N);
 
-    struct mahasiswa kelas[N];
-
-    for (int i = 0; i < N; i++){
-        printf("Nama siswa : ");
-        scanf("%s", &kelas[i].nama);
-        printf("NIM siswa : ");
-        scanf("%d", &kelas[i].nim);
-        printf("Kehadiran (dalam persen) : ");
-        scanf("%d", &kelas[i].kehadiran);
+    if(N < 1){
+        printf("Input jumlah mahasiswa tidak valid!");
     }
-    printf("\nDaftar mahasiswa yang kehadirannya dibawah 80% : \n\n");
+    else{
+        struct mahasiswa kelas[N];
 
-    for (int i = 0; i < N; i++){
-            if(kelas[i].kehadiran < 80){
-                printf("Nama : %s NIM : %d\n", kelas[i].nama, kelas[i].nim);
+        for (int i = 0; i < N; i++){
+            printf("Nama siswa : ");
+            scanf("%s", &kelas[i].nama);
+            printf("NIM siswa : ");
+            scanf("%d", &kelas[i].nim);
+            printf("Kehadiran (dalam persen) : ");
+            scanf("%d", &kelas[i].kehadiran);
+            while(kelas[i].kehadiran < 0 || kelas[i].kehadiran > 100){
+                printf("\nPersen kehadiran tidak valid!\nKehadiran (dalam persen) : ");
+                scanf("%d", &kelas[i].kehadiran);
             }
+        }
+        printf("\nDaftar mahasiswa yang kehadirannya dibawah 80% : \n\n");
+
+        for (int i = 0; i < N; i++){
+                if(kelas[i].kehadiran < 80){
+                    printf("Nama : %s NIM : %d\n", kelas[i].nama, kelas[i].nim);
+                }
+        }
+
     }
+
+    return 0;
 }
